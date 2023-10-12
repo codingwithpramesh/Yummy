85fd27acf5236ef093f24b71b720a6b944313d4b
Pramesh <codingwithpramesh@gmail.com>
2023-10-12 13:06:25 +0545

    save

--- YummyM/Controllers/EcomPortfolioController.cs
+++ YummyM/Controllers/EcomPortfolioController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Net.Http.Headers;
 using System.Text;
 using YummyM.Models;
@@ -12,6 +13,8 @@ namespace YummyM.Controllers
         Uri baseAddress = new Uri("https://localhost:7115/api/");
         private readonly HttpClient _httpClient;
         private readonly IWebHostEnvironment _webHostEnvironment;
+
+ 
         public EcomPortfolioController( IWebHostEnvironment webHostEnvironment)
         {
             _webHostEnvironment = webHostEnvironment;
@@ -45,6 +48,7 @@ namespace YummyM.Controllers
             {
                 try
                 {
+
                     string url = baseAddress + "EcomPortfolio/Create";
                     var multipartContent = new MultipartFormDataContent();
                     if (ecom.ImageAbout != null)
@@ -79,47 +83,82 @@ namespace YummyM.Controllers
                         multipartContent.Add(streamContent, "AboutVideos", ecom.VideosAbout.FileName);
                     }
 
-
-                    
                     //StringContent content = new StringContent(JsonConvert.SerializeObject(ecom));
-                    multipartContent.Add(new StringContent(ecom.HeroTitle), "HeroTitle");
-                    multipartContent.Add(new StringContent(ecom.HeroDescription), "HeroDescription");
-                    multipartContent.Add(new StringContent(ecom.HeroButton), "HeroButton");
-                    multipartContent.Add(new StringContent(ecom.AboutTitle), "AboutTitle");
-                    multipartContent.Add(new StringContent(ecom.AboutDescription), "AboutDescription");
-                    /*multipartContent.Add(new StringContent(ecom.CardTitle), "CardTitle");
-                    multipartContent.Add(new StringContent(ecom.CardDescription), "CardDescription");
-                    multipartContent.Add(new StringContent(ecom.CardButtonText), "CardButtonText");
-                    multipartContent.Add(new StringContent(ecom.TotalClient), "TotalClient");
-                    multipartContent.Add(new StringContent(ecom.totalProject), "totalProject");
-                    multipartContent.Add(new StringContent(ecom.TotalHours), "TotalHours");
-                    multipartContent.Add(new StringContent(ecom.TotalWorkers), "TotalWorkers");
-                    multipartContent.Add(new StringContent(ecom.MenuTitle), "MenuTitle");
-                    multipartContent.Add(new StringContent(ecom.Menuvalue), "Menuvalue");
-                    multipartContent.Add(new StringContent(ecom.TimeCategory.ToString()), "TimeCategory");
-                    multipartContent.Add(new StringContent(ecom.professorTitle), "professorTitle");
+                    multipartContent.Add(new StringContent(ecom.HeroTitle ?? string.Empty), "HeroTitle");
+                    multipartContent.Add(new StringContent(ecom.HeroDescription ?? string.Empty), "HeroDescription");
+                    multipartContent.Add(new StringContent(ecom.HeroButton ?? string.Empty), "HeroButton");
+                    multipartContent.Add(new StringContent(ecom.AboutTitle ?? string.Empty), "AboutTitle");
+                    multipartContent.Add(new StringContent(ecom.AboutDescription ?? string.Empty), "AboutDescription");
+                    multipartContent.Add(new StringContent(ecom.CardTitle ?? string.Empty), "CardTitle");
+                    multipartContent.Add(new StringContent(ecom.CardDescription ?? string.Empty), "CardDescription");
+                    multipartContent.Add(new StringContent(ecom.CardButtonText?? string.Empty), "CardButtonText");
+                    multipartContent.Add(new StringContent(ecom.TotalClient ?? string.Empty), "TotalClient");
+                    multipartContent.Add(new StringContent(ecom.totalProject ?? string.Empty), "totalProject");
+                    multipartContent.Add(new StringContent(ecom.TotalHours ?? string.Empty), "TotalHours");
+                    multipartContent.Add(new StringContent(ecom.TotalWorkers ?? string.Empty), "TotalWorkers");
+                    multipartContent.Add(new StringContent(ecom.MenuTitle ?? string.Empty), "MenuTitle");
+                    multipartContent.Add(new StringContent(ecom.Menuvalue ?? string.Empty), "Menuvalue");
+                    multipartContent.Add(new StringContent(ecom.TimeCategory.ToString() ?? string.Empty), "TimeCategory");
+                    multipartContent.Add(new StringContent(ecom.professorTitle ?? string.Empty), "professorTitle");
+                    multipartContent.Add(new StringContent(ecom.ProfessorDescription ?? string.Empty), "HeroTitle");
+                    multipartContent.Add(new StringContent(ecom.HeroDescription ?? string.Empty), "HeroDescription");
+                    multipartContent.Add(new StringContent(ecom.HeroButton ?? string.Empty), "HeroButton");
+                    multipartContent.Add(new StringContent(ecom.AboutTitle ?? string.Empty), "AboutTitle");
+                    multipartContent.Add(new StringContent(ecom.AboutDescription ?? string.Empty), "AboutDescription");
+                    multipartContent.Add(new StringContent(ecom.CardTitle ?? string.Empty), "CardTitle");
+                    multipartContent.Add(new StringContent(ecom.CardDescription ?? string.Empty), "CardDescription");
+                    multipartContent.Add(new StringContent(ecom.CardButtonText ?? string.Empty), "CardButtonText");
+                    multipartContent.Add(new StringContent(ecom.TotalClient ?? string.Empty), "TotalClient");
+                    multipartContent.Add(new StringContent(ecom.totalProject ?? string.Empty), "totalProject");
+                    multipartContent.Add(new StringContent(ecom.TotalHours ??string.Empty), "TotalHours");
+                    multipartContent.Add(new StringContent(ecom.TotalWorkers ?? string.Empty), "TotalWorkers");
+                    multipartContent.Add(new StringContent(ecom.MenuTitle ?? string.Empty), "MenuTitle");
+                    multipartContent.Add(new StringContent(ecom.Menuvalue ?? string.Empty), "Menuvalue");
+                    multipartContent.Add(new StringContent(ecom.TimeCategory.ToString() ?? string.Empty), "TimeCategory");
+                    multipartContent.Add(new StringContent(ecom.professorTitle ?? string.Empty), "professorTitle");
+                    multipartContent.Add(new StringContent(ecom.professorName ?? string.Empty), "professorName");
+                    multipartContent.Add(new StringContent(ecom.ProfessorPosition ?? string.Empty), "ProfessorPosition");
+                    multipartContent.Add(new StringContent(ecom.ProfessorRating ?? string.Empty), "ProfessorRating");
+                    multipartContent.Add(new StringContent(ecom.eventtitle ?? string.Empty), "eventtitle");
+                    multipartContent.Add(new StringContent(ecom.eventDescription ?? string.Empty), "eventDescription");
+                    multipartContent.Add(new StringContent(ecom.EventPrice ?? string.Empty), "EventPrice");
+                    multipartContent.Add(new StringContent(ecom.eventDescrip ?? string.Empty), "eventDescrip");
+                    multipartContent.Add(new StringContent(ecom.chefTitle ?? string.Empty), "chefTitle");
+                    multipartContent.Add(new StringContent(ecom.chefName ?? string.Empty), "chefName");
+                    multipartContent.Add(new StringContent(ecom.ChefPosition ?? string.Empty), "ChefPosition");
+                    multipartContent.Add(new StringContent(ecom.ChefDescription ??string.Empty), "ChefDescription");
+                    multipartContent.Add(new StringContent(ecom.GalleryTitle ?? string.Empty), "GalleryTitle");
+                    multipartContent.Add(new StringContent(ecom.ContactTitle ?? string.Empty), "ContactTitle");
+                    multipartContent.Add(new StringContent(ecom.ContactDescription ?? string.Empty), "ContactDescription");
+                    multipartContent.Add(new StringContent(ecom.contactEmail ?? string.Empty), "contactEmail");
+                    multipartContent.Add(new StringContent(ecom.contactAddress ?? string.Empty), "contactAddress");
+                    multipartContent.Add(new StringContent(ecom.Phone ?? string.Empty), "Phone");
+                    multipartContent.Add(new StringContent(ecom.OpeningHour ?? string.Empty), "OpeningHour");
+                    multipartContent.Add(new StringContent(ecom.FooterAddress ?? string.Empty), "FooterAddress");
+                    multipartContent.Add(new StringContent(ecom.FooterPhone ?? string.Empty), "FooterPhone");
+                    multipartContent.Add(new StringContent(ecom.FooterEmail ??string.Empty), "FooterEmail");
+                    multipartContent.Add(new StringContent(ecom.FooterOpeningHour ?? string.Empty), "FooterOpeningHour");
 
-                    multipartContent.Add(new StringContent(ecom.ProfessorDescription), "HeroTitle");
-                    multipartContent.Add(new StringContent(ecom.HeroDescription), "HeroDescription");
-                    multipartContent.Add(new StringContent(ecom.HeroButton), "HeroButton");
-                    multipartContent.Add(new StringContent(ecom.AboutTitle), "AboutTitle");
-                    multipartContent.Add(new StringContent(ecom.AboutDescription), "AboutDescription");
-                    multipartContent.Add(new StringContent(ecom.CardTitle), "CardTitle");
-                    multipartContent.Add(new StringContent(ecom.CardDescription), "CardDescription");
-                    multipartContent.Add(new StringContent(ecom.CardButtonText), "CardButtonText");
-                    multipartContent.Add(new StringContent(ecom.TotalClient), "TotalClient");
-                    multipartContent.Add(new StringContent(ecom.totalProject), "totalProject");
-                    multipartContent.Add(new StringContent(ecom.TotalHours), "TotalHours");
-                    multipartContent.Add(new StringContent(ecom.TotalWorkers), "TotalWorkers");
-                    multipartContent.Add(new StringContent(ecom.MenuTitle), "MenuTitle");
-                    multipartContent.Add(new StringContent(ecom.Menuvalue), "Menuvalue");
-                    multipartContent.Add(new StringContent(ecom.TimeCategory.ToString()), "TimeCategory");
-                    multipartContent.Add(new StringContent(ecom.professorTitle), "professorTitle");*/
-                    if (ecom.ImageAbout != null)
-                    {
-                        multipartContent.Add(new StringContent(ecom.ImageAbout.FileName), "ImageAbout");
-                    }
-                    multipartContent.Add(new StringContent(ecom.AboutDescription), "AboutDescription");
+                    /*  await using var stream = System.IO.File.OpenRead("./Test.txt");
+                      *//*string filepath = @"D:\Yummy\Yummy\YummyAPI\wwwroot\Images\"; *//*
+                      string filename = Path.GetFileName(ecom.ImageAbout.ToString());
+                      if (ecom.ImageAbout != null)
+                      {
+
+                          multipartContent.Add(new StreamContent(stream), "ImageAbout", "Test.txt");
+                      }*/
+
+
+                   
+                    var fileroute = ecom.ImageEvent.FileName;
+                    var filename = Path.GetFileName(fileroute);
+                    using var FileStream = System.IO.File.OpenRead(fileroute);
+                    multipartContent.Add(new StreamContent(FileStream), "ImageAbout" ,filename);
+                   //multipartContent.Add(new StreamContent(FileStream), "VideosAbout", filename);
+                  /*  multipartContent.Add(new StreamContent(FileStream), "ImageEvent", filename);
+                    multipartContent.Add(new StreamContent(FileStream), "Imagechef", filename);
+                    multipartContent.Add(new StreamContent(FileStream), "ImageGallery", filename);*/
+                    multipartContent.Add(new StringContent(ecom.AboutDescription ?? string.Empty), "AboutDescription");
                    
          
 
@@ -221,6 +260,19 @@ namespace YummyM.Controllers
 
 
             return NotFound();
+        }
+
+
+        private async Task FileSave(IFormFile file)
+        {
+            string wwwroot = _webHostEnvironment.WebRootPath;
+            var filepath = Path.Combine(wwwroot, "Files" , file.FileName);
+            using (var fileStream = new FileStream(filepath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            
         }
     }
 }
